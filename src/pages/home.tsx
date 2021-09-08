1c3134ffacf7fd962b89afc84a19c1f95ec0be88
sheben404 <sheben404@gmail.com>
2021-09-09 01:32:20 +0800

    style: 引入 header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './home.scss';
+import headerBg from '../images/header.png';
 
 export const Home = () => {
   return (
     <div className={'home'}>
-      Home 页面
+      <header style={{backgroundImage:`url(${headerBg})`}}></header>
     </div>
   );
 };
